0f3ec448bf727db0405cd9f38cd764ec00f4347a
josh99smith <josh99smith@gmail.com>
2020-11-17 15:50:10 -0500

    added comments and notes

--- logic.js
+++ logic.js
@@ -3,30 +3,31 @@ var units; //number of units in a serving size
 var weight; //weight of a serving size in grams
 var submit; //submit button for form
 
-function getValues()
+function getValues() //function runs when user presses submit button
 {
-  breadtype = document.getElementById("grain_bread").value;
-  units = document.getElementById("units").value;
-  weight = document.getElementById("weight").value;
+  breadtype = document.getElementById("grain_bread").value; //assigns the type of bread variable based on user input from html form
+  units = document.getElementById("units").value; //assigns the number  units variable based on user input from html form
+  weight = document.getElementById("weight").value; //assigns the weight per serving variable based on user input from html form
   console.log(breadtype);
   
-  if ([
+  if ([ //array of food items (Group A)
        'Bread sticks (hard)',
        'Crackers, Savory (club, cheese, ritz, saltine)',
        'Croutons',
        'Chow mein noodles',
        'Pretzels (hard)',
        'Stuffing (dry) Note: weights apply to bread in stuffing.'
-    ].indexOf(breadtype) >= 0) 
-      {
-        cal = Math.ceil(weight / units);
-        preschoolageresult = 11 / cal;
-        schoolageresult = 22 / cal;  
-        document.getElementById("15results").innerHTML = Math.ceil(preschoolageresult);
-        document.getElementById("612results").innerHTML = Math.ceil(schoolageresult);
+    ].indexOf(breadtype) >= 0) //checks breadtype selected by user in html form, runs calculation only if user has selected one of the items in the array
+      {  //calculations to output ounce equivalency based on user input
+        cal = Math.ceil(weight / units); //rounds up the result of weight variable divided by unit variable
+        preschoolageresult = 11 / cal; //assigns ounce equivalency value for preschool aged child
+        schoolageresult = 22 / cal;  //assigns ounce equivalency value for preschool aged child
+        document.getElementById("15results").innerHTML = Math.ceil(preschoolageresult);  //update HTML table with preschool age ounce equivalency results of calculation
+        document.getElementById("612results").innerHTML = Math.ceil(schoolageresult); //update HTML table with school age ounce equivalency results of calculation
      }
   
-  else if ([
+     //same logic repeats for each bread category type 
+  else if ([ //array of food items (Group B)
       'Bagels',
       'Biscuits',
       'Bread sticks (soft)',
@@ -51,7 +52,7 @@ function getValues()
 
 
   
-  else if ([
+  else if ([ //array of food items (Group C)
       'Corn muffins',
       'Cornbread',
       'Croissants',
@@ -67,7 +68,7 @@ function getValues()
         document.getElementById("612results").innerHTML = Math.ceil(schoolageresult);
      }
 
-  else if ([
+  else if ([ //array of food items (Group D)
       'Muffins (all, except corn)'
     ].indexOf(breadtype) >= 0) 
      {
@@ -77,8 +78,8 @@ function getValues()
         document.getElementById("15results").innerHTML = Math.ceil(preschoolageresult);
         document.getElementById("612results").innerHTML = Math.ceil(schoolageresult);
      }
-     
-  else if ([
+
+  else if ([ //array of food items (Group E)
       'French toast'
     ].indexOf(breadtype) >= 0)
      {
@@ -89,7 +90,7 @@ function getValues()
         document.getElementById("612results").innerHTML = Math.ceil(schoolageresult);
      }
   
-  else if ([
+  else if ([ //array of food items (Group H)
       'Breakfast cereals (cooked)',
       'Noodles (all varieties)',
       'Pasta (all shapes)',
@@ -104,7 +105,7 @@ function getValues()
         document.getElementById("612results").innerHTML = Math.ceil(schoolageresult);
      }
   
-  else if ([
+  else if ([ //array of food items (Group I)
       'Breakfast cereals - Flakes (ready to eat)',
       'Breakfast cereals - Granola (ready to eat)',
       'Breakfast cereals - Puffed (ready to eat)',
@@ -118,6 +119,6 @@ function getValues()
         document.getElementById("612results").innerHTML = Math.ceil(schoolageresult);
      }
   
-  else {console.log("No Value");}
+  else {console.log("No Value");} //returns only if item selected from dropdown has no ounce ounce equivalency converstion formula yet
   
 };
