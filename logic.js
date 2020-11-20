33b291058223b7a20b973d62db5158db92daae9e
josh99smith <josh99smith@gmail.com>
2020-11-20 07:15:02 -0500

    reduced line count & consolidated formulas

--- logic.js
+++ logic.js
@@ -1,34 +1,24 @@
-var breadtype; //type of grain
-var units; //number of units in a serving size
-var weight; //weight of a serving size in grams
-var submit; //submit button for form
+// var breadtype;    //type of grain   ---> these 4 aren't needed because I defined them in the function below and converted them to let. also 'var submit;' has no use. - ZO
+// var units;    //number of units in a serving size
+// var weight;    //weight of a serving size in grams
+// var submit;    //submit button for form
 
-function getValues() //function runs when user presses submit button
-{
-  breadtype = document.getElementById("grain_bread").value; //assigns the type of bread variable based on user input from html form
-  units = document.getElementById("units").value; //assigns the number  units variable based on user input from html form
-  weight = document.getElementById("weight").value; //assigns the weight per serving variable based on user input from html form
-  console.log(breadtype);
-  
-  if ([ //array of food items (Group A)
-       'Bread sticks (hard)',
-       'Crackers, Savory (club, cheese, ritz, saltine)',
-       'Croutons',
-       'Chow mein noodles',
-       'Pretzels (hard)',
-       'Stuffing (dry) Note: weights apply to bread in stuffing.'
-    ].indexOf(breadtype) >= 0) //checks breadtype selected by user in html form, runs calculation only if user has selected one of the items in the array
-      {  //calculations to output ounce equivalency based on user input
-        cal = Math.ceil(weight / units); //rounds up the result of weight variable divided by unit variable
-        preschoolageresult = 11 / cal; //assigns ounce equivalency value for preschool aged child
-        schoolageresult = 22 / cal;  //assigns ounce equivalency value for preschool aged child
-        document.getElementById("15results").innerHTML = Math.ceil(preschoolageresult);  //update HTML table with preschool age ounce equivalency results of calculation
-        document.getElementById("612results").innerHTML = Math.ceil(schoolageresult); //update HTML table with school age ounce equivalency results of calculation
-     }
-  
-     //same logic repeats for each bread category type 
-  else if ([ //array of food items (Group B)
-      'Bagels',
+const getValues = function () {    //function runs when user presses submit button
+   let breadtype = document.getElementById("grain_bread").value;    //assigns the type of bread variable based on user input from html form
+   let units = document.getElementById("units").value;    //assigns the number  units variable based on user input from html form
+   let weight = document.getElementById("weight").value;    //assigns the weight per serving variable based on user input from html form
+
+   if (['Bread sticks (hard)',   //array of food items (Group A)
+      'Crackers, Savory (club, cheese, ritz, saltine)',
+      'Croutons',
+      'Chow mein noodles',
+      'Pretzels (hard)',
+      'Stuffing (dry) Note: weights apply to bread in stuffing.'
+   ].indexOf(breadtype) >= 0) {    //checks breadtype selected by user in html form, runs calculation only if user has selected one of the items in the array
+      preschoolOE = 11    //assigns ounce equivalency value based on USDA conversion for preschool aged child
+      schoolAgeOE = 22    //assigns ounce equivalency value based on USDA conversion for preschool aged child
+
+   } else if (['Bagels', //array of food items (Group B)
       'Biscuits',
       'Bread sticks (soft)',
       'Breads (white, wheat, whole wheat, French, Italian)',
@@ -36,89 +26,62 @@ function getValues() //function runs when user presses submit button
       'Crackers, Sweet (graham or animal) - all shapes',
       'English muffins',
       'Pita bread (white, wheat, whole wheat)',
-      'Pizza crust','Pretzels (soft)',
+      'Pizza crust', 'Pretzels (soft)',
       'Rolls (white, wheat, whole wheat, potato)',
       'Taco shells',
       'Tortilla chips (whole corn or enriched)',
       'Tortillas (wheat or corn)'
-    ].indexOf(breadtype) >= 0)
-      {
-        cal = Math.ceil(weight / units);
-        preschoolageresult = 14 / cal;
-        schoolageresult = 28 / cal;  
-        document.getElementById("15results").innerHTML = Math.ceil(preschoolageresult);
-        document.getElementById("612results").innerHTML = Math.ceil(schoolageresult);
-     }
+   ].indexOf(breadtype) >= 0) {
+      preschoolOE = 14    
+      schoolAgeOE = 28    
 
-
-  
-  else if ([ //array of food items (Group C)
+   } else if ([   //array of food items (Group C)
       'Corn muffins',
       'Cornbread',
       'Croissants',
       'Pancakes',
       'Pie crusts (savory, meat/meat alternate pies)',
       'Waffles'
-    ].indexOf(breadtype) >= 0)
-      {
-        cal = Math.ceil(weight / units);
-        preschoolageresult = 17 / cal;
-        schoolageresult = 34 / cal;  
-        document.getElementById("15results").innerHTML = Math.ceil(preschoolageresult);
-        document.getElementById("612results").innerHTML = Math.ceil(schoolageresult);
-     }
+   ].indexOf(breadtype) >= 0) {
+      preschoolOE = 17    
+      schoolAgeOE = 34    
 
-  else if ([ //array of food items (Group D)
-      'Muffins (all, except corn)'
-    ].indexOf(breadtype) >= 0) 
-     {
-        cal = Math.ceil(weight / units);
-        preschoolageresult = 28 / cal;
-        schoolageresult = 55 / cal;  
-        document.getElementById("15results").innerHTML = Math.ceil(preschoolageresult);
-        document.getElementById("612results").innerHTML = Math.ceil(schoolageresult);
-     }
+   } else if (['Muffins (all, except corn)'].indexOf(breadtype) >= 0) {    //array of food items (Group D)
+      preschoolOE = 28    
+      schoolAgeOE = 55    
 
-  else if ([ //array of food items (Group E)
-      'French toast'
-    ].indexOf(breadtype) >= 0)
-     {
-        cal = Math.ceil(weight / units);
-        preschoolageresult = 35 / cal;
-        schoolageresult = 69 / cal;  
-        document.getElementById("15results").innerHTML = Math.ceil(preschoolageresult);
-        document.getElementById("612results").innerHTML = Math.ceil(schoolageresult);
-     }
-  
-  else if ([ //array of food items (Group H)
+   } else if (['French toast'].indexOf(breadtype) >= 0) {   //array of food items (Group E)
+      preschoolOE = 35    
+      schoolAgeOE = 69    
+
+   } else if ([ //array of food items (Group H)
       'Breakfast cereals (cooked)',
       'Noodles (all varieties)',
       'Pasta (all shapes)',
       'Quinoa',
       'Rice (enriched white or brown)'
-    ].indexOf(breadtype) >= 0) 
-     {
-        cal = Math.ceil(weight / units);
-        preschoolageresult = 14 / cal;
-        schoolageresult = 28 / cal;  
-        document.getElementById("15results").innerHTML = Math.ceil(preschoolageresult);
-        document.getElementById("612results").innerHTML = Math.ceil(schoolageresult);
-     }
-  
-  else if ([ //array of food items (Group I)
+   ].indexOf(breadtype) >= 0) {
+      preschoolOE = 14   
+      schoolAgeOE = 28    
+
+   } else if ([ //array of food items (Group I)
       'Breakfast cereals - Flakes (ready to eat)',
       'Breakfast cereals - Granola (ready to eat)',
       'Breakfast cereals - Puffed (ready to eat)',
       'Breakfast cereals - Rounds (ready to eat)'
-    ].indexOf(breadtype) >= 0)  
-     {
-        cal = Math.ceil(weight / units);
-        preschoolageresult = 17 / cal;
-        schoolageresult = 34 / cal;  
-        document.getElementById("15results").innerHTML = Math.ceil(preschoolageresult);
-        document.getElementById("612results").innerHTML = Math.ceil(schoolageresult);
-     }
-  
-  else {console.log("No Value");} //returns only if item selected from dropdown has no ounce ounce equivalency converstion formula yet
-  
-};
+   ].indexOf(breadtype) >= 0) {
+      preschoolOE = 17
+      schoolAgeOE = 34
+
+   } else {
+      console.log("No Value");    //returns only if item selected from dropdown has no ounce ounce equivalency converstion formula yet
+   }
+
+   //Calculations for
+   cal = Math.ceil(weight / units);    //rounds up the result of weight variable divided by unit variable --- //calculations to output ounce equivalency based on user input
+   preschoolAgeResult = preschoolOE / cal;    //assigns ounce equivalency value for preschool aged child
+   schoolAgeResult = schoolAgeOE / cal;    //assigns ounce equivalency value for preschool aged child
+   document.getElementById("15results").innerHTML = Math.ceil(preschoolAgeResult);        //update HTML table with preschool age ounce equivalency results of calculation
+   document.getElementById("612results").innerHTML = Math.ceil(schoolAgeResult);       //update HTML table with school age ounce equivalency results of calculation
+
+}
